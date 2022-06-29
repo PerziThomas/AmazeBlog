28361f0f7122a72ab6086d0a154ffaf9849755bb
Dimenzio <Dimenzioderechte@gmail.com>
2022-06-29 22:50:11 +0200

    Init backend

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World! I am running!");
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Express Server running and listening on http://${HOST}:${PORT}`);
+});
